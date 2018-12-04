321dacf35a02b026d91ff21d2352744ff93f0a21
Olaf Schouten <olafschouten99@gmail.com>
2018-12-04 12:33:31 +0100

    Tot level 7

--- Week_11-14/adventure.js
+++ Week_11-14/adventure.js
@@ -6,21 +6,14 @@ var button1 = document.getElementById("button1");
 var button2 = document.getElementById("button2");
 var button3 = document.getElementById("button3");
 
-var inventoryItem = document.getElementById('inventoryItem');
-
-var inventory = { 'Sleutel': false }
-
-
-function itemSleutel() {
-    inventory['Sleutel'] = true;
-    document.getElementById('inventoryItem').style.display = 'none';
-}
-
+var inventoryItem = document.getElementById('inventoryItem')
 
 
 //---------------startMenu---------------//
 
 function startMenu() {
+    gamecontainer.classList.add('backgroundStartMenu');
+    gamecontainer.style.backgroundImage = "url('images/bg_startmenu.jpg')";
     title.classList.add('title_Postion');
     button1.innerHTML = "StartGame";
     button1.classList.add('button1_Postion');
@@ -28,11 +21,10 @@ function startMenu() {
     button3.style.display = 'none';
     inventoryItem.style.display = 'none';
     button1.onclick = level1;
-    gamecontainer.classList.add('backgroundStartMenu');
-    gamecontainer.style.backgroundImage = "url('images/bg_startmenu.jpg')";
 }
 
 function level1() {
+    gamecontainer.classList.remove('backgroundStartMenu');
     gamecontainer.classList.add('backgroundLevel1');
     title.style.display = 'none';
     description.innerHTML = 'Kies welke kan je op wilt'
@@ -49,6 +41,7 @@ function level1() {
 }
 
 function dood() {
+    gamecontainer.classList.remove('backgroundLevel1');
     gamecontainer.classList.add('backgroundYoudied');
     description.style.display = 'none';
     gamecontainer.classList.add('youdied');
@@ -60,90 +53,48 @@ function dood() {
 }
 
 function level2() {
+    gamecontainer.classList.remove('backgroundStartMenu');
     gamecontainer.classList.add('backgroundLevel2');
-    inventoryItem.classList.add('itemEen');
-    inventoryItem.src = "images/sleutel.png";
-    inventoryItem.style.display = 'inline-block';
     description.style.display = 'none';
     gamecontainer.style.backgroundImage = "url('images/bg_lv2.jpg')";
     button1.innerHTML = 'Terug';
     button1.onclick = level1;
     button2.style.display = 'none';
     button3.style.display = 'none';
-
-    inventoryItem.onclick = itemSleutel;
 }
 
 function level3() {
+    gamecontainer.classList.remove('backgroundLevel2');
     gamecontainer.classList.add('backgroundLevel3');
     description.style.display = 'none';
     gamecontainer.style.backgroundImage = "url('images/bg_lv3.jpg')";
     button1.innerHTML = 'Links';
     button1.onclick = level1;
     button2.style.display = 'inline-block';
-    button2.onclick = level6;
-    button2.innerHTML = 'Vooruit';
-    button3.style.display = 'inline-block';
-    button3.innerHTML = 'Rechts';
-    button3.onclick = level4;
+    button3.style.display = 'none';
+    button2.innerHTML = 'Rechts';
+    button2.onclick = level4;
+
 }
 
 function level4() {
+    gamecontainer.classList.remove('backgroundLevel3');
     gamecontainer.classList.add('backgroundLevel4');
     description.style.display = 'none';
     gamecontainer.style.backgroundImage = "url('images/bg_lv4.jpg')";
-    button1.onclick = level3;
-    button1.innerHTML = 'Links';
-    button2.innerHTML = 'Vooruit';
-    button2.onclick = level5;
-    button2.style.display = 'inline-block';
+    button1.innerHTML = 'Vooruit';
+    button1.onclick = level5;
+    button2.style.display = 'none';
     button3.style.display = 'none';
 }
 
 function level5() {
+    gamecontainer.classList.remove('backgroundLevel4');
     gamecontainer.classList.add('backgroundLevel5');
-    description.style.display = ('Zoek een sleutel om binnen te komen.');
+    description.style.display = 'none';
     gamecontainer.style.backgroundImage = "url('images/bg_lv5.jpg')";
-    button1.innerHTML = 'Links';
-    button1.onclick = level4;
-    button2.style.display = 'inline-block';
-    button2.onclick = level5;
-    button3.style.display = 'none';
-
-    if (inventory['Sleutel'] == true) {
-        button2.onclick = level7;
-    }
-    else {
-        button2.onclick = level6_1;
-    }
-}
-
-function level6() {
-    gamecontainer.classList.add('backgroundLevel6');
-    description.style.display = 'none';
-    gamecontainer.style.backgroundImage = "url('images/bg_lv6.jpg')";
-    button1.innerHTML = 'Terug';
-    button1.onclick = level3;
-    button2.style.display = 'none';
-    button3.style.display = 'none';
-}
-
-function level6_1() {
-    gamecontainer.classList.add('backgroundLevel6_1');
-    description.style.display = 'none';
-    gamecontainer.style.backgroundImage = "url('images/bg_lv6_1.jpg')";
-    button1.innerHTML = 'Terug';
-    button1.onclick = level6;
-    button2.style.display = 'none';
-    button3.style.display = 'none';
-}
-
-function level7() {
-    gamecontainer.classList.add('backgroundLevel7');
-    description.style.display = 'none';
-    gamecontainer.style.backgroundImage = "url('images/bg_lv7.jpg')";
-    button1.innerHTML = 'Terug';
-    button1.onclick = level6;
+    button1.innerHTML = 'Vooruit';
+    // button1.onclick = level5;
     button2.style.display = 'none';
     button3.style.display = 'none';
 }

5a3a1f1bbdc22204c8a9fe67431026d8bf74e83d
Olaf Schouten <olafschouten99@gmail.com>
2019-01-22 12:00:15 +0100

    Lingo win einde

--- Week_14-18/B2W19O1a - Lingo/Lingo/js/main.js
+++ Week_14-18/B2W19O1a - Lingo/Lingo/js/main.js
@@ -22,6 +22,7 @@ for (b = 1; b <= 5; b++) {
 	container.appendChild(input);
 	input.classList.add("input");
 	input.setAttribute('placeholder', 'voer je antwoord in');
+	input.setAttribute('maxLength', '5');
 
 	//---- Divjes aanmaken met een class en een id ----//
 	for (var i = 0; i < 5; i++) {
@@ -37,7 +38,7 @@ document.getElementById("row1").firstElementChild.innerHTML = randomWordArray[0]
 
 var userInput;
 
-//---- Als je op enter drukt word die in de blokjes geplaatst ----//
+//---- Voor deze function uit ----//
 input.onkeypress = function (event) {
 	if (event.key == "Enter" || event.keyCode == 13) {
 		var woord = input.value;
@@ -63,33 +64,38 @@ input.onkeypress = function (event) {
 
 function check2() {
 
-	var randomWordPartsCopy = randomWordArray.split("");
-
-	//---- Controleren van het ingevoerde woord (5 letters) ----//
-	for (var i = 0; i < 5; i++) {
-		document.getElementById("row" + (activerow - 1) + "box" + i).classList.add("red");
-
-		//---- Maakt de letters op de goede plek null ----//
-		if (randomWordParts[i] == userInput[i]) {
-			goed[i] = randomWordParts[i];
-			document.getElementById("row" + (activerow - 1) + "box" + i).style.backgroundColor = "green";
-			randomWordPartsCopy[i] = null;
-		}
-
-		//---- Doet anders niets ----//
-		else {
-			fout[i] = userInput[i];
-		}
+	if (input.value.length != 5) {
+		return alert('Minimaal 5 letters invoeren');
 	}
-	i++;
+	else {
+		var randomWordPartsCopy = randomWordArray.split("");
 
-	//---- Loop voor het controleren of dat de letter in het woord zitten ----//
-	for (j = 0; j < fout.length; j++) {
+		//---- Controleren van het ingevoerde woord (5 letters) ----//
+		for (var i = 0; i < 5; i++) {
+			document.getElementById("row" + (activerow - 1) + "box" + i).classList.add("red");
 
-		//---- Als de letter in het woord zit dat je invoerd word hij geel ----//
-		if (fout[j] != null && randomWordPartsCopy.indexOf(fout[j]) > -1) {
-			document.getElementById("row" + (activerow - 1) + "box" + j).style.backgroundColor = "yellow";
+			//---- Maakt de letters op de goede plek null ----//
+			if (randomWordParts[i] == userInput[i]) {
+				goed[i] = randomWordParts[i];
+				document.getElementById("row" + (activerow - 1) + "box" + i).style.backgroundColor = "green";
+				randomWordPartsCopy[i] = null;
+			}
 		}
+		i++;
+
+		//---- Loop voor het controleren of dat de letter in het woord zitten ----//
+		for (j = 0; j < fout.length; j++) {
+
+			//---- Als de letter in het woord zit dat je invoerd word hij geel ----//
+			if (fout[j] != null && randomWordPartsCopy.indexOf(fout[j]) > -1) {
+				document.getElementById("row" + (activerow - 1) + "box" + j).style.backgroundColor = "yellow";
+			}
+		}
+		j++;
 	}
-	j++;
+
+	if (randomWordParts[i] == userInput[i]) {
+		alert('Je hebt gewonnen');
+	}
+
 }

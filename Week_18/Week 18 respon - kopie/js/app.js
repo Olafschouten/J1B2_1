cd39d925d1e05703a4cabe93457ef920cafc2ebb
Olafschouten <olafschouten99@gmail.com>
2019-01-29 21:03:50 +0100

    Responsive

---
+++ Week_18/Week 18 respon - kopie/js/app.js
@@ -0,0 +1,7 @@
+function drop1() {
+    document.getElementById('contactDropdown').classList.toggle('show');
+}
+
+function drop2() {
+    document.getElementById('linksDropdown').classList.toggle('show');
+}
